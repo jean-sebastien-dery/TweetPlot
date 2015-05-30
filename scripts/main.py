24464d0f9d8b2fd2e2fbf776f6ae0b4041b5ad5e
Jean-Sebastien Dery <jeansebastien@dery.com>
2015-05-30 15:17:18 -0400

    added TwitterAuthParams to increase modularity

--- scripts/main.py
+++ scripts/main.py
@@ -1,11 +1,14 @@
-'''
+"""
 Created on May 21, 2015
 
-@author: jsdery
-'''
+@author: Jean-Sebastien Dery
+"""
 
 from tweetplot.twitter_account import TwitterAccount 
+from tweetplot.twitter_account import TwitterAuthParams
 import logging
+
+_CONFIG_FILE_NAME = "twitterAuthInformation.ini"
 
 if __name__ == '__main__':
     
@@ -15,7 +18,8 @@ if __name__ == '__main__':
     logging.info("About to authenticate with Twitter and get the neighbors of your vertex in the 'friends' graph.")
     
     try:
-        twitter_account = TwitterAccount()
+        twitter_auth_params = TwitterAuthParams(_CONFIG_FILE_NAME)
+        twitter_account = TwitterAccount(twitter_auth_params)
         auth_user_id = twitter_account.getAuthenticatedUserId()
         neighbors_list = twitter_account.getListOfFriendsFromId(user_id=auth_user_id)
     
@@ -30,5 +34,6 @@ if __name__ == '__main__':
             print("There are '"+str(len(neighbors_of_neighbor))+"' neighbors of neighbors that were added to the adjacency list.")
     except Exception as exception:
         # TODO: Will potentially need to handle different types of exceptions.
-        print(exception.args)
+        print("An exception ocured while creating the graph of neighbors.")
+        print(exception)
         
--- scripts/tweetplot/twitter_account.py
+++ scripts/tweetplot/twitter_account.py
@@ -1,47 +1,34 @@
-'''
+"""
 Created on May 21, 2015
 
-@author: jsdery
-'''
+@author: Jean-Sebastien Dery
+"""
 
 import tweepy
 import ConfigParser
 import os.path
 
-class TwitterAccount(object):
-    '''
-    classdocs
-    '''
+class TwitterAuthParams(object):
     
-    _CONFIG_FILE_NAME = "twitterAuthInformation.ini"
-    _CONFIG_SECTION_NAME = "TwitterScan.Auth"
+    _CONFIG_SECTION_NAME = "TweetPlot.Auth"
     _CONSUMER_KEY_VAR_NAME = "consumer_key"
     _CONSUMER_SECRET_VAR_NAME = "consumer_secret"
     _ACCESS_TOKEN_KEY_VAR_NAME = "access_token_key"
     _ACCESS_TOKEN_SECRET_VAR_NAME = "access_token_secret"
-
-    def __init__(self):
-        '''
-        Constructor
-        '''        
-        
-        self._getAuthParamsFromConfig()
+    
+    def __init__(self, config_file_name):
+        self._getAuthParamsFromConfig(config_file_name=config_file_name)
         
         if self._areAuthParamsMissing():
-            auth = tweepy.OAuthHandler(self._consumer_key, self._consumer_secret)
-            auth.secure = True
-            auth.set_access_token(self._access_token_key, self._access_token_secret)
-            self._twitterApi = tweepy.API(auth)
-        else:
             raise ValueError("At least one authentication parameter from the config file is missing.", self._CONSUMER_KEY_VAR_NAME+"="+self._consumer_key, self._CONSUMER_SECRET_VAR_NAME+"="+self._consumer_secret, self._ACCESS_TOKEN_KEY_VAR_NAME+"="+self._access_token_key, self._ACCESS_TOKEN_SECRET_VAR_NAME+"="+self._access_token_secret)
         
-    def _getAuthParamsFromConfig(self):
+    def _getAuthParamsFromConfig(self, config_file_name):
         self._configFile = ConfigParser.ConfigParser()
         
-        if (not os.path.isfile(self._CONFIG_FILE_NAME)):
+        if (not os.path.isfile(config_file_name)):
             raise ValueError("The authentication config file cannot be found.", "Name of config = "+self._CONFIG_FILE_NAME)
         
-        self._configFile.read(self._CONFIG_FILE_NAME)
+        self._configFile.read(config_file_name)
         
         self._consumer_key = self._configFile.get(self._CONFIG_SECTION_NAME, self._CONSUMER_KEY_VAR_NAME)
         self._consumer_secret = self._configFile.get(self._CONFIG_SECTION_NAME, self._CONSUMER_SECRET_VAR_NAME)
@@ -51,9 +38,35 @@ class TwitterAccount(object):
     
     def _areAuthParamsMissing(self):
         if (not self._consumer_key) or (not self._consumer_secret) or (not self._access_token_key) or (not self._access_token_secret):
-            return False
-        else:
             return True
+        else:
+            return False
+        
+    def getConsumerKey(self):
+        return self._consumer_key
+    
+    def getConsumerSecret(self):
+        return self._consumer_secret
+    
+    def getAccessTokenKey(self):
+        return self._access_token_key
+    
+    def getAccessTokenSecret(self):
+        return self._access_token_secret
+
+class TwitterAccount(object):
+    '''
+    classdocs
+    '''
+    
+    def __init__(self, twitter_auth_params):
+        '''
+        Constructor
+        '''        
+        auth = tweepy.OAuthHandler(twitter_auth_params.getConsumerKey(), twitter_auth_params.getConsumerSecret())
+        auth.secure = True
+        auth.set_access_token(twitter_auth_params.getAccessTokenKey(), twitter_auth_params.getAccessTokenSecret())
+        self._twitterApi = tweepy.API(auth)
     
     def getAuthenticatedUserId(self):
         return self._twitterApi.me().id
